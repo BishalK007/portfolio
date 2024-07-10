da94b78798ee170c2700a294bdfd1bc8bec20bf9
bishal <bishalkarmakar007@gmail.com>
2024-07-10 18:02:35 +0530

    update data

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,6 @@ import React from 'react'
 import * as yaml from 'js-yaml';
 import * as fs from 'fs';
 import '@styles/globals.css'
-import WelcomeScreen from '@/components/welcome_screen';
-import AboutMe from '@components/aboutme';
-import Experience from '@components/experience';
-import Projects from '@components/projects';
-import GetContact from '@components/get-contact';
-import Footer from '@components/footer';
-import Nav from '@components/nav';
 import GlobalWrapper from '@components/global-wrapper';
 
 
