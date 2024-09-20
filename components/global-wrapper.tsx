20b08a02fa265122f7b24bba8ee6d98a038ce3db
Your Name <you@example.com>
2024-09-20 22:24:06 +0530

    Background edited TODO customization

--- components/global-wrapper.tsx
+++ components/global-wrapper.tsx
@@ -7,8 +7,9 @@ import Experience from './experience';
 import Projects from './projects';
 import GetContact from './get-contact';
 import Footer from './footer';
-import { ToastContainer} from 'react-toastify'
+import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
+import BgTheme from './bg-theme';
 
 const GlobalWrapper: React.FC<{ data: Data }> = ({ data }) => {
   const sectionRefs = {
@@ -18,15 +19,17 @@ const GlobalWrapper: React.FC<{ data: Data }> = ({ data }) => {
     projects: useRef<HTMLDivElement>(null),
     getcontact: useRef<HTMLDivElement>(null),
   };
-  
+  const foregroundRef = useRef<HTMLDivElement>(null)
+  const [foregroundHeight, setForegroundHeight] = useState('0')
+
   const [currViewPortItem, setCurrViewPortItem] = useState('');
 
   useEffect(() => {
-    // console.log(currViewPortItem)
+    // Intersection Observer setup
     const options = {
       root: null, // Use the viewport as the root
       rootMargin: '0px',
-      threshold: 0.4, // When 60% of the element is visible
+      threshold: 0.4, // When 40% of the element is visible
     };
 
     const callback: IntersectionObserverCallback = (entries) => {
@@ -46,32 +49,49 @@ const GlobalWrapper: React.FC<{ data: Data }> = ({ data }) => {
       }
     });
 
+    // Set the foreground height using offsetHeight
+    if (foregroundRef.current) {
+      setForegroundHeight(`${foregroundRef.current.offsetHeight}px`);
+    }
+
     return () => {
       // Cleanup observer on component unmount
       observer.disconnect();
     };
-  }, [sectionRefs]);
+  }, [sectionRefs, foregroundRef]);
 
   return (
-    <div>
-      <Nav currSelectedItem={currViewPortItem} />
-      <div ref={sectionRefs.home} id="home">
-        <WelcomeScreen data={data} />
+    <div className='w-full h-[100dvh] overflow-auto'>
+      {/* background */}
+      <div
+        className="w-full absolute overflow-hidden"
+        style={{
+          height: `${foregroundHeight}`
+        }}
+      >
+        <BgTheme />
       </div>
-      <div ref={sectionRefs.aboutme} id="aboutme">
-        <AboutMe data={data} />
+      {/* Foreground */}
+      <div className="absolute z-10 w-full" ref={foregroundRef}>
+        <Nav currSelectedItem={currViewPortItem} />
+        <div ref={sectionRefs.home} id="home">
+          <WelcomeScreen data={data} />
+        </div>
+        <div ref={sectionRefs.aboutme} id="aboutme">
+          <AboutMe data={data} />
+        </div>
+        <div ref={sectionRefs.experience} id="experience">
+          <Experience data={data} />
+        </div>
+        <div ref={sectionRefs.projects} id="projects">
+          <Projects data={data} />
+        </div>
+        <div ref={sectionRefs.getcontact} id="get-contact">
+          <GetContact data={data} />
+        </div>
+        <Footer data={data} />
+        <ToastContainer />
       </div>
-      <div ref={sectionRefs.experience} id="experience">
-        <Experience data={data} />
-      </div>
-      <div ref={sectionRefs.projects} id="projects">
-        <Projects data={data} />
-      </div>
-      <div ref={sectionRefs.getcontact} id="get-contact">
-        <GetContact data={data} />
-      </div>
-      <Footer data={data} />
-      <ToastContainer />
     </div>
   );
 };
