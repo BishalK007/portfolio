94bfa5094fc3c41817ca616b9b526840380f4d1a
Bishal <bishal@bishal.pro>
2025-07-13 15:33:59 +0530

    UPDATE metadata on main page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,21 @@
 
-export const metadata = {
-  title: 'Bishal ',
+
+export const metadata: Metadata = {
+  title: 'Bishal',
   description: 'Welcome to My Portfolio',
-}
+  keywords: 'CV, resume, Bishal, Karmakar, full stack developer, portfolio, download resume',
+  openGraph: {
+    title: 'Bishal',
+    description: 'Bishal Karmakar, a software engineer and full-stack developer specializing in modern web technologies.',
+    type: 'website',
+    url: '/cv',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Bishal',
+    description: 'Bishal Karmakar, a software engineer and full-stack developer specializing in modern web technologies.',
+  },
+};
 
 export default function RootLayout({
   children,
